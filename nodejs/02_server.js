0dc17f406c6851355a38e1050ff1aa4262251995
bob <474234165@qq.com>
2022-03-28 15:44:31 +0800

    foo

--- nodejs/02_server.js
+++ nodejs/02_server.js
@@ -1,5 +1,6 @@
 var http = require('http');
-http.createServer(function(request,response){
-    response.writeHead(200,{'Content-Type':'text/plain'});
+http.createServer(function(request, response) {
+    // 内容写在{}中
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World\n');
 }).listen(8888);
